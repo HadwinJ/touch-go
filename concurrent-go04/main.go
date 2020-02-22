f31568ec2a832b16eade95d2d4e848bbf8833f6e
hjjaws <hjjaws@amazon.com>
2020-02-22 20:36:06 +0800

    implement select statement

--- concurrent-go04/main.go
+++ concurrent-go04/main.go
@@ -2,28 +2,67 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
+
+var cache = map[int]Book{}
+var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan int, 1)
+	m := &sync.RWMutex{}
+	cacheCh := make(chan Book)
+	dbCh := make(chan Book)
 
-	wg.Add(2)
-	go func(ch <-chan int, wg *sync.WaitGroup) {
-		for msg := range ch {
-			fmt.Println(msg)
-		}
-		wg.Done()
-	}(ch, wg)
+	for i := 0; i < 10; i++ {
+		id := rnd.Intn(10) + 1
+		wg.Add(2)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
+			if b, ok := queryCache(id, m); ok {
+				ch <- b
+			}
+			wg.Done()
+		}(id, wg, m, cacheCh)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
+			if b, ok := queryDatabase(id, m); ok {
+				m.Lock()
+				cache[id] = b
+				m.Unlock()
+				ch <- b
+			}
+			wg.Done()
+		}(id, wg, m, dbCh)
 
-	go func(ch chan<- int, wg *sync.WaitGroup) {
-		for i := 0; i < 10; i++ {
-			ch <- i
-		}
-		close(ch)
-		wg.Done()
-	}(ch, wg)
-
+		go func(cacheCh, dbCh <-chan Book) {
+			select {
+			case b := <-cacheCh:
+				fmt.Println("from cache")
+				fmt.Println(b)
+				<-dbCh
+			case b := <-dbCh:
+				fmt.Println("from database")
+				fmt.Println(b)
+			}
+		}(cacheCh, dbCh)
+	}
 	wg.Wait()
+}
+
+func queryCache(id int, m *sync.RWMutex) (Book, bool) {
+	m.RLock()
+	b, ok := cache[id]
+	m.RUnlock()
+	return b, ok
+}
+
+func queryDatabase(id int, m *sync.RWMutex) (Book, bool) {
+	time.Sleep(100 * time.Millisecond)
+	for _, b := range books {
+		if b.ID == id {
+			return b, true
+		}
+	}
+	return Book{}, false
 }
