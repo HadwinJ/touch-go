77ca57b7ba3b9ae3292f9827256dcc6a6088a6e1
hjjaws <hjjaws@amazon.com>
2020-02-22 20:10:49 +0800

    get 0 when receive from close channel

--- concurrent-go04/main.go
+++ concurrent-go04/main.go
@@ -10,15 +10,15 @@ func main() {
 	ch := make(chan int, 1)
 
 	wg.Add(2)
-	go func(ch <-chan int, wg *sync.WaitGroup) {
+	go func(ch chan int, wg *sync.WaitGroup) {
+		fmt.Println(<-ch)
+		close(ch)
 		fmt.Println(<-ch)
 		wg.Done()
 	}(ch, wg)
 
 	go func(ch chan<- int, wg *sync.WaitGroup) {
 		ch <- 42
-		close(ch)
-		ch <- 27
 		wg.Done()
 	}(ch, wg)
 
