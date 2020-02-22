e0f968a3ccb3d4a4e445bc1ae26504b22e764bc8
hjjaws <hjjaws@amazon.com>
2020-02-22 20:05:35 +0800

    send and receive channel

--- concurrent-go04/main.go
+++ concurrent-go04/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
@@ -11,16 +10,13 @@ func main() {
 	ch := make(chan int, 1)
 
 	wg.Add(2)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch <-chan int, wg *sync.WaitGroup) {
 		fmt.Println(<-ch)
-		ch <- 27
 		wg.Done()
 	}(ch, wg)
 
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch chan<- int, wg *sync.WaitGroup) {
 		ch <- 42
-		time.Sleep(5 * time.Millisecond)
-		fmt.Println(<-ch)
 		wg.Done()
 	}(ch, wg)
 
