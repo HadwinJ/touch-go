b9a21df1c664ebe7b2110d06381ee9addf342662
hjjaws <hjjaws@amazon.com>
2020-02-22 20:16:54 +0800

    If statement - to detect channel

--- concurrent-go04/main.go
+++ concurrent-go04/main.go
@@ -10,15 +10,16 @@ func main() {
 	ch := make(chan int, 1)
 
 	wg.Add(2)
-	go func(ch chan int, wg *sync.WaitGroup) {
-		fmt.Println(<-ch)
-		close(ch)
-		fmt.Println(<-ch)
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		if msg, ok := <-ch; ok {
+			fmt.Println(msg, ok) // ok = FALSE means closed, TRUE means open
+		}
 		wg.Done()
 	}(ch, wg)
 
 	go func(ch chan<- int, wg *sync.WaitGroup) {
-		ch <- 42
+		close(ch)
+		// ch <- 0
 		wg.Done()
 	}(ch, wg)
 
